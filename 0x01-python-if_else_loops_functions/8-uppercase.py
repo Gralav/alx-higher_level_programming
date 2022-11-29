da3b223dc7fdea479f17462c2beb1e2d7a8f261f
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-11-29 12:49:24 +0300

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        i = ord(i)
-        if 97 <= i <= 122:
-            i = i - 32
-        print("{:c}".format(i), end='')
-    print()
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
