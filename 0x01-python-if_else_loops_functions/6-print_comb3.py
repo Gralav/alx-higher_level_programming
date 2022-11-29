cd3cadacfccd1c4ab99543bb15a6644b87399ea4
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-11-29 13:14:12 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
-""" #!/usr/bin/python3 """
-for i in range(0, 10):
-    for j in range(0, 10):
-        if (i * 10 + j) == 89:
-            print("{:02d}".format(i * 10 + j))
-        elif (i * 10 + j) < (j * 10 + i):
-            print("{:02d}, ".format(i * 10 + j), end='')
+#!/usr/bin/python3
+for num in range(0, 10):
+    for num2 in range(0, 10):
+        if num is 8 and num2 is 9:
+            print("{:d}{:d}".format(num, num2))
+        elif num < num2:
+            print("{:d}{:d}".format(num, num2), end=", ")
