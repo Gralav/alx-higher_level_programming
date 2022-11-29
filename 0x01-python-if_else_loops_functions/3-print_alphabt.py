aeead2edb275ae7ddc6dd3d4a67ae4b8db2d5079
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-11-29 12:41:44 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for a in range(97, 97 + 26):
-    if a == 113 or a == 101:
-        continue
-    print("{:c}".format(a), end='')
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
+
