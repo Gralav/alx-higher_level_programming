79c8be2c65b02383d840285bda4de8457d108bb2
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-02 05:05:45 +0300

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
