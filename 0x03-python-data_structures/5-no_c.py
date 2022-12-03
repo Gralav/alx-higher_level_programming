355519ffd68259297e8ae76648a277d320863b3c
Gralav <lavenderwaga@gmail.com>
2022-12-03 09:51:35 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 def no_c(my_string):
-    new_string = ''
-    for i in my_string:
-        if i in ['c', 'C']:
-            continue
-        new_string = new_string + i
-        return new_string
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
 
