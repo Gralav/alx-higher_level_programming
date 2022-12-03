a8c771ebf7a0afc481bb9625e813dbe7025fff41
Gralav <lavenderwaga@gmail.com>
2022-12-03 11:04:48 -0800

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for count, j in enumerate(i):
-            print("{:d}".format(j), end="")
-            if count != (len(i) - 1):
-                print("{}".format(" "), end="")
-                print()
+        print(" ".join("{:d}".format(j) for j in i))
