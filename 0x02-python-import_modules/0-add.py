7cb7364c3a7ee836f252728cf09b0090827fa614
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-06 12:19:53 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+# 0_add.py
+# Gralav <lavenderwaga@.com>
+
 if __name__ == "__main__":
-	from add_0 import add
-	a = 1
-	b = 2
-	print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
