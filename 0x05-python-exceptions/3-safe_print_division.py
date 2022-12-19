72939d5200eb1e9e33aaae6deb1f078e7e226f6c
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-19 07:41:39 +0300

    Create 3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+ * safe_print_division - Function that divides 2 integers and prints the
+ *                       result.
+ *
+ * @a: This is the input first number
+ * @b: This is the input second number
+ *
+ * Return: The value of the division, otherwise: None
+"""
+
+
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return 
