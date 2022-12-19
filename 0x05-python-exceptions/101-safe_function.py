e5578bc651d23977b73f2de95a13bdc1feca067c
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-19 20:32:51 +0300

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
-     """Executes a function safely.
+    """Executes a function safely.
     Args:
         fct: The function to execute.
         args: Arguments for fct.
