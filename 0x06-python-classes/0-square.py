801acdbf04ad5e98eb1c18cc16e94ecaee09b196
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-20 21:26:11 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+
+"""Write an empty class Square that defines a square"""
+
 
 class Square:
-    """Represent a square."""
+    """ Pass Works to do nothing in the class"""
+
     pass
