bf9ecb6117395a34029499b9a3043e8b70cdba30
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2022-12-20 21:27:34 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+write a function that prints the first x elements of a list and only integers.
+"""
+
+
+class Square:
+    """Initialize a class with init"""
+    def __init__(self, size):
+        self.__size = 
