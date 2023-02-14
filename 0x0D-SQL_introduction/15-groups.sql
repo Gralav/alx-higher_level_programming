8959cbda5098aa1def36ac838fa60fa376a19aae
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-02-14 15:02:14 +0300

    Create 15-groups.sql

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+-- Script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
+SELECT `score`, COUNT(*) AS "number" FROM second_table GROUP BY `score` HAVING COUNT(*) > 0 ORDER BY `number` DESC;
