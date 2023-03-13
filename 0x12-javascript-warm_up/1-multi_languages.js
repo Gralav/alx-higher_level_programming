618e63e0685b6bd45d1927048a311c801902b64f
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-13 23:27:28 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-myArray.forEach(element => console.log(element));
+/* Script that prints 3 lines:(like '1-multi_languajes.js') but using an array
+ * of string and a loop */
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myArray[i] !== undefined; i++) {
+  console.log(myArray[i]);
+}
