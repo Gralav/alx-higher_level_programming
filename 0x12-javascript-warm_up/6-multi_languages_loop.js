8fd5941ca7e09f27efd0a1f2fab51b56f32f33b4
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-13 23:29:31 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let i = 0;
-while (i < 3) {
+/* Script that prints 3 lines:(like '1-multi_languajes.js') but using an array
+ * of string and a loop */
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; myArray[i] !== undefined; i++) {
   console.log(myArray[i]);
-  i++;
 }
