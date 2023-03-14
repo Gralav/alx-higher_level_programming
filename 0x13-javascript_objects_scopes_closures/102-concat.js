3ae40792227390d341db2a9a98cbbd86b7765e27
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-14 17:21:38 +0300

    Create 102-concat.js

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+let first = "";
+let second = "";
+fs.readFile('hola', 'utf-8', (err, data) =>{
+  if (err) throw err;
+  first = data;
+  second = first + first + first;
+  console.log(second);
+});
