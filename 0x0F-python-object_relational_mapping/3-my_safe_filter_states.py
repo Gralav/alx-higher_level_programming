38e9295a92bf339fb78fa22e72ff1a975a8bac8c
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-17 02:58:40 +0300

    Create 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script thak takes in an argument and displays all values in the states
+    table of 'hbtn_0e_0_usa' where name matches the argument. """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",  # your host
+                         user=argv[1],       # username
+                         passwd=argv[2],     # password
+                         db=argv[3],
+                         port=3306)   # name of the database
+    cur = db.cursor()
+
+    cur.execute("""SELECT * FROM states
+                   WHERE name = %s ORDER BY id ASC""", (argv[4],))
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
