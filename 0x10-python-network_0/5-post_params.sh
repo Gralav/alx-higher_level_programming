3ec40d7b47665c1a41b172f7e3ea1ae989c62640
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-30 23:07:41 +0300

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a POST request to the URL and displays the body of the response
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD"
