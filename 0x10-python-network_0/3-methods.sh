16f36e32a781eaf4306bf340b9e78c79729bb66d
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-30 22:51:14 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+# Bash script that takes in a URL and displays all HTTP methods the server will accept
+curl -sI $1 | grep "Allow" | cut -d' ' -f2-
