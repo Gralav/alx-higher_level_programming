dd2bf2e429d706af959bcbf80ac2c8a80318eed0
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-31 12:20:47 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Python script that fetches https://intranet.hbtn.io/status
+Python script that fetches https://alx-intranet.hbtn.io/status
 """
 if __name__ == "__main__":
 
     import urllib.request
 
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
