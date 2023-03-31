63927b3ca6a2aa193ee172804d47ecf8e8d2c097
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-03-31 06:47:47 +0300

    Create 2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the
+response (decoded in utf-8)
+"""
+if __name__ == "__main__":
+
+    import urllib.parse
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        info = response.read()
+        print(info.decode('utf-8'))
