c0df1efc4fcf1ca19456da56c768f43ca679d8de
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-26 07:02:32 -0400

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,16 @@
 #!/usr/bin/node
+/*
+ * Script that prints the title of a Star Wars movie where the episode number
+ * matches a given integer.
+*/
+
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+
+request(url, function (err, result, body) {
+  console.log(JSON.parse(body).title);
+  if (err) {
+    console.log(err);
+  }
 });
