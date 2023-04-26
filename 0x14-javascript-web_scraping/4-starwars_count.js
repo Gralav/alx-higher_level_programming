750714804b5bf73186ceb21401fbd113e82a85a8
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-26 07:04:11 -0400

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,22 @@
 #!/usr/bin/node
-
-const myArray = process.argv.slice(2);
+/*
+ * Script that prints the number of movies where the character "Wedge Antilles"
+ * is present.
+*/
+const url = process.argv[2];
 const request = require('request');
 
-request(myArray[0], function (error, response, body) {
-  if (error) { console.log(error); } else {
-    const results = JSON.parse(body).results;
+request(url, function (err, result, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const films = JSON.parse(body).results;
     let count = 0;
-    if (results && results.length >= 1) {
-      results.forEach(dict => { if (dict.characters.includes('https://swapi.co/api/people/18/')) { count++; } });
+    for (let i = 0; i < films.length; i++) {
+      const tempCount = films[i].characters.toString().match(/18/g);
+      if (tempCount !== null) {
+        count = count + tempCount.length;
+      }
     }
     console.log(count);
   }
