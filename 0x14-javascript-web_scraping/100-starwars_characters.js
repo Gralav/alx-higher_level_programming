58b538093f6c5a0a1fea9e91f84e21b173048225
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-25 23:02:13 -0400

    Create 100-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const myArray = process.argv.slice(2);
+const request = require('request');
+
+if (Number.isInteger(parseInt(myArray[0]))) {
+  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
+    if (error) console.error('error:', error);
+    JSON.parse(body).characters.forEach(link => request(link, (error, response, body) => {
+      if (error) console.error('error:', error);
+      console.log(JSON.parse(body).name);
+    }));
+  });
+}
