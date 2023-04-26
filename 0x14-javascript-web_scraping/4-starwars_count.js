92a746759fc278bf4cfcb5387afc84c8a3e19214
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-26 07:05:49 -0400

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,23 +1,29 @@
 #!/usr/bin/node
 /*
- * Script that prints the number of movies where the character "Wedge Antilles"
- * is present.
+ * Script that computes the number of tasks completed by user ID
+ *
 */
 const url = process.argv[2];
 const request = require('request');
+const tasksDict = {};
 
 request(url, function (err, result, body) {
   if (err) {
     console.log(err);
   } else {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (let i = 0; i < films.length; i++) {
-      const tempCount = films[i].characters.toString().match(/18/g);
-      if (tempCount !== null) {
-        count = count + tempCount.length;
+    const tasks = JSON.parse(body);
+
+    for (let i = 0; i < tasks.length; i++) {
+      if (!tasks[i].completed) {
+        continue;
       }
+      const userId = tasks[i].userId;
+      if (userId in tasksDict) {
+        tasksDict[userId] += 1;
+        continue;
+      }
+      tasksDict[userId] = 1;
     }
-    console.log(count);
   }
+  console.log(tasksDict);
 });
