8b18d107e898da7b6f6bd94faa48fb313676d70f
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-26 06:41:11 -0400

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,25 @@
 #!/usr/bin/node
+import requests
+import sys
 
-const myArray = process.argv.slice(2);
-const request = require('request');
+def get_movie_title(movie_id):
+    url = f"https://swapi-api.alx-tools.com/api/films/{movie_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        movie = response.json()
+        return movie['title']
+    else:
+        return None
 
-if (Number.isInteger(parseInt(myArray[0]))) {
-  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
-    if (error) console.error('error:', error); // Print the error if one occurred
-    console.log(JSON.parse(body).title);
-  });
-}
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python swapi.py <movie_id>")
+        sys.exit(1)
+
+    movie_id = int(sys.argv[1])
+    title = get_movie_title(movie_id)
+    if title:
+        print(title)
+    else:
+        print(f"No movie found with ID {movie_id}")
+
