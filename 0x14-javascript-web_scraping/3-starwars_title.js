e8fe0f1d04a86e703ddb1a884e6b73cdf5317bf1
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-04-25 23:04:19 -0400

    Create 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const myArray = process.argv.slice(2);
+const request = require('request');
+
+if (Number.isInteger(parseInt(myArray[0]))) {
+  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
+    if (error) console.error('error:', error); // Print the error if one occurred
+    console.log(JSON.parse(body).title);
+  });
+}
