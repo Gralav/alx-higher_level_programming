ce51f0a47bcb5c807b8ccbe88cf985ab37f53c3e
Grace  Lavender Waga <101356786+Gralav@users.noreply.github.com>
2023-05-04 13:28:04 -0400

    Create 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,4 @@
+/* Script that updates the text color of the <header> element to red (#FF0000) */
+document.addEventListener("DOMContentLoaded", function () {
+	document.querySelector("header").style.color = "#FF0000";
+});
